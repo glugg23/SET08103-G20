ebfd888c2513a07e32f24b09363add915bc3db83
glugg23 <m.leonhardt424@gmail.com>
2019-01-19 15:13:36 +0000

    Connection to mariadb working on localhost

--- src/main/java/com/napier/group20/Main.java
+++ src/main/java/com/napier/group20/Main.java
@@ -1,7 +1,24 @@
 package com.napier.group20;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:mariadb://localhost:3306/world?user=root");
+            String query = "SELECT * FROM country;";
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
+
+            while(resultSet.next()) {
+                System.out.println(resultSet.getString("Name"));
+            }
+
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
     }
 }
