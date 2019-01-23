75357271244c5904a93ff10c659aac1bd8e3ccb9
wrestlerdude <raishallan2@gmail.com>
2019-01-23 11:59:35 +0000

    Started docker-compose. View TODO.

--- src/main/java/com/napier/group20/Main.java
+++ src/main/java/com/napier/group20/Main.java
@@ -8,7 +8,7 @@ import java.sql.Statement;
 public class Main {
     public static void main(String[] args) {
         try {
-            Connection connection = DriverManager.getConnection("jdbc:mariadb://localhost:3306/world?user=root");
+            Connection connection = DriverManager.getConnection("jdbc:mariadb://localhost:3306/world", "root", "password");
             String query = "SELECT * FROM country;";
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery(query);
