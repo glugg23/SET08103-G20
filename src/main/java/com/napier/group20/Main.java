5a274573b126066c2479935bc620c7a1f8a50264
wrestlerdude <raishallan2@gmail.com>
2019-01-25 00:27:34 +0000

    Added sql volume to docker-compose, adjusted timeout, added. Rollback docker-compose, adjusted travis, logged into database as root rather than custom user.

--- src/main/java/com/napier/group20/Main.java
+++ src/main/java/com/napier/group20/Main.java
@@ -8,7 +8,7 @@ import java.sql.Statement;
 public class Main {
     public static void main(String[] args) {
         try {
-            Connection connection = DriverManager.getConnection("jdbc:mariadb://db:3306/world", "user", "pwd");
+            Connection connection = DriverManager.getConnection("jdbc:mariadb://db:3306/world", "root", "secret");
             String query = "SELECT * FROM country;";
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery(query);
