caecd36f4722de85dc04c389867892cd2100e557
wrestlerdude <raishallan2@gmail.com>
2019-01-25 16:43:41 +0000

    Made database connection order dependent on the jar, rather than very long health-checks. Note: This could make the docker-compose work as the latest version.

--- src/main/java/com/napier/group20/Main.java
+++ src/main/java/com/napier/group20/Main.java
@@ -1,5 +1,6 @@
 package com.napier.group20;
 
+import java.util.concurrent.TimeUnit;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -7,8 +8,16 @@ import java.sql.Statement;
 
 public class Main {
     public static void main(String[] args) {
+        Connection connection = null;
+        while (connection == null) {
+            try {
+                TimeUnit.SECONDS.sleep(5);
+                connection = DriverManager.getConnection("jdbc:mariadb://db:3306/world", "root", "secret");
+            } catch (Exception e) {
+                System.err.println("Connection to MariaDB container failed, retrying.");
+            }
+        }
         try {
-            Connection connection = DriverManager.getConnection("jdbc:mariadb://db:3306/world", "root", "secret");
             String query = "SELECT * FROM country;";
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery(query);
@@ -16,7 +25,6 @@ public class Main {
             while(resultSet.next()) {
                 System.out.println(resultSet.getString("Name"));
             }
-
         } catch(Exception e) {
             e.printStackTrace();
         }
