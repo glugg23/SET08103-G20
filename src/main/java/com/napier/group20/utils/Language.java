beca310a9517473dfc36869942caea6f471e6e47
MrRa1n <tobycookni@gmail.com>
2019-02-04 22:07:25 +0000

    Implemented Language class

---
+++ src/main/java/com/napier/group20/utils/Language.java
@@ -0,0 +1,34 @@
+package com.napier.group20.utils;
+
+/**
+ * Language.java
+ *
+ * Stores the language information of a country, including the
+ * language name, the country code, what percentage of people speak it
+ * and if it is the official language
+ *
+ */
+
+public class Language {
+
+    private String languageName;
+    private String countryCode;
+    private double percentage;
+    private boolean isOfficial;
+
+    public String getLanguageName() {
+        return languageName;
+    }
+
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    public double getPercentage() {
+        return percentage;
+    }
+
+    public boolean isOfficial() {
+        return isOfficial;
+    }
+}
