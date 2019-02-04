ecafc4e225b2c4c7326b6492f4b4f754e54934dc
MrRa1n <tobycookni@gmail.com>
2019-02-04 22:10:38 +0000

    Added Continent skeleton - Regions class is required to implement properties

---
+++ src/main/java/com/napier/group20/utils/Continent.java
@@ -0,0 +1,12 @@
+package com.napier.group20.utils;
+
+/**
+ * Continent.java
+ *
+ * Stores information on continents including name,
+ * list of regions and the population
+ *
+ */
+public class Continent {
+
+}
