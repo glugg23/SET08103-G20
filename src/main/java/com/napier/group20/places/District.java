756742baa301e6029ed0326b6e60fde3f6303d6d
Jacob Barrow <barrow.jacob@gmail.com>
2019-02-06 11:07:07 +0000

    Added District class

---
+++ src/main/java/com/napier/group20/places/District.java
@@ -0,0 +1,38 @@
+package com.napier.group20.places;
+
+import com.napier.group20.utils.Population;
+
+import java.util.ArrayList;
+
+/**
+ * District.java
+ *
+ * Stores the attributes of , including the
+ * language name, the country code, what percentage of people speak it
+ * and if it is the official language
+ *
+ */
+
+public class District implements Population {
+
+    private String name;
+    private ArrayList<City> cities;
+
+    public District(String name, ArrayList<City> cities) {
+        this.name = name;
+        this.cities = cities;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<City> getCities() {
+        return cities;
+    }
+
+    public long getPopulation() {
+        return 0;
+    }
+
+}
