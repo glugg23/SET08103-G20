b9c3abb9c8ff60e2ddeb8346fc07ce364c65d95b
Jacob Barrow <barrow.jacob@gmail.com>
2019-02-06 11:00:55 +0000

    Added Country class

---
+++ src/main/java/com/napier/group20/places/Country.java
@@ -0,0 +1,58 @@
+package com.napier.group20.places;
+
+import com.napier.group20.utils.Population;
+
+import java.util.ArrayList;
+
+/**
+ * Country.java
+ *
+ * Stores the country's attributes
+ *
+ */
+
+public class Country implements Population {
+
+    private String countryCode;
+    private String name;
+    private Continent continent;
+    private Region region;
+    private ArrayList<District> districts;
+    private City capital;
+    private ArrayList<Language> language;
+
+
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Continent getContinent() {
+        return continent;
+    }
+
+    public Region getRegion() {
+        return region;
+    }
+
+    public ArrayList<District> getDistricts() {
+        return districts;
+    }
+
+    public City getCapital() {
+        return capital;
+    }
+
+    public ArrayList<Language> getLanguage() {
+        return language;
+    }
+
+    public long getPopulation() {
+        return 0;
+    }
+
+
+}
