9ce68237cb8fc09cb83abadc42a38f45717d4f8b
Jacob Barrow <barrow.jacob@gmail.com>
2019-02-09 13:51:27 +0000

    Added world class

---
+++ src/main/java/com/napier/group20/places/World.java
@@ -0,0 +1,27 @@
+package com.napier.group20.places;
+
+import com.napier.group20.utils.Population;
+
+import java.util.ArrayList;
+
+/**
+ * World.java
+ *
+ * Stores the attributes of the world
+ *
+ *
+ */
+
+public class World implements Population {
+
+    private ArrayList<Continent> continents;
+
+    public ArrayList<Continent> getContinents() {
+        return continents;
+    }
+
+    public long getPopulation() {
+        return 0;
+    }
+
+}
