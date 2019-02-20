1b08b0b235c43f30590899b483fecf220addeff8
glugg23 <m.leonhardt424@gmail.com>
2019-02-20 18:29:54 +0000

    Added basic unit test layout

---
+++ src/test/java/com/napier/group20/places/CityTest.java
@@ -0,0 +1,27 @@
+package com.napier.group20.places;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CityTest {
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void getCountry() {
+    }
+
+    @Test
+    void getDistrict() {
+    }
+
+    @Test
+    void getPopulation() {
+    }
+
+    @Test
+    void isCapital() {
+    }
+}
