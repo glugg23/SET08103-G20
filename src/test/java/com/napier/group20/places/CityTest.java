db3a3bbe8d48da8222465d33e66cc3b9bdc859d1
glugg23 <m.leonhardt424@gmail.com>
2019-02-24 16:48:52 +0000

    Completed City unit tests

--- src/test/java/com/napier/group20/places/CityTest.java
+++ src/test/java/com/napier/group20/places/CityTest.java
@@ -7,21 +7,49 @@ class CityTest {
 
     @Test
     void getName() {
+        //Arrange
+        City city = new City("City", null, null, 100, false);
+
+        //Assert
+        assertEquals("City", city.getName());
     }
 
     @Test
     void getCountry() {
+        //Arrange
+        Country country = new Country("ABC", "Country", null, null, null,
+                null, null, 200);
+        City city = new City("City", country, null, 100, false);
+
+        //Assert
+        assertEquals(country, city.getCountry());
     }
 
     @Test
     void getDistrict() {
+        //Arrange
+        District district = new District("District", null);
+        City city = new City("City", null, district, 100, false);
+
+        //Assert
+        assertEquals(district, city.getDistrict());
     }
 
     @Test
     void getPopulation() {
+        //Arrange
+        City city = new City("City", null, null, 100, false);
+
+        //Assert
+        assertEquals(100, city.getPopulation());
     }
 
     @Test
     void isCapital() {
+        //Arrange
+        City city = new City("City", null, null, 100, true);
+
+        //Assert
+        assertTrue(city.isCapital());
     }
 }
