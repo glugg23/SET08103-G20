643d26e8e2148ec7f291d272585f63884deb2cb5
MrRa1n <tobycookni@gmail.com>
2019-02-25 22:00:07 +0000

    Implemented test for getCountryCode method

--- src/test/java/com/napier/group20/places/CountryTest.java
+++ src/test/java/com/napier/group20/places/CountryTest.java
@@ -4,9 +4,14 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CountryTest {
+
     @Test
     void getCountryCode() {
-
+        // Arrange
+        Country country = new Country("ABC", "Country", null, null, null,
+                null, null, 12345);
+        // Assert
+        assertEquals("ABC", country.getCountryCode());
     }
 
     @Test
