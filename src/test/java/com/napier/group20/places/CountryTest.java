7af53e11e989645200222393dc71f713727ced1d
MrRa1n <tobycookni@gmail.com>
2019-02-25 16:11:42 +0000

    Created signatures for Country Test

---
+++ src/test/java/com/napier/group20/places/CountryTest.java
@@ -0,0 +1,46 @@
+package com.napier.group20.places;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CountryTest {
+    @Test
+    void getCountryCode() {
+
+    }
+
+    @Test
+    void getName() {
+
+    }
+
+    @Test
+    void getContinent() {
+
+    }
+
+    @Test
+    void getRegion() {
+
+    }
+
+    @Test
+    void getDistricts() {
+
+    }
+
+    @Test
+    void getPopulation() {
+
+    }
+
+    @Test
+    void getCapital() {
+
+    }
+
+    @Test
+    void getOfficialLanguage() {
+
+    }
+}
