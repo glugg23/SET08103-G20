0baee72e1c158f444506b5aff982786e6bc9b414
MrRa1n <tobycookni@gmail.com>
2019-02-27 11:06:45 +0000

    Added singleton design pattern for world

--- src/main/java/com/napier/group20/places/World.java
+++ src/main/java/com/napier/group20/places/World.java
@@ -13,6 +13,17 @@ import java.util.ArrayList;
 
 public class World implements Population {
 
+    private World() {}
+
+    private static World instance = null;
+
+    public static World getInstance() {
+        if (instance  == null) {
+            instance = new World();
+        }
+        return instance;
+    }
+
     private ArrayList<Continent> continents;
 
     public ArrayList<Continent> getContinents() {
