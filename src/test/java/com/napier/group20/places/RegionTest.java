881c6afa85353bb2f57530b06aa9b941dc227652
Jacob Barrow <barrow.jacob@gmail.com>
2019-02-27 11:34:31 +0000

    Added region test

---
+++ src/test/java/com/napier/group20/places/RegionTest.java
@@ -0,0 +1,45 @@
+package com.napier.group20.places;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RegionTest {
+
+    @Test
+    void getName() {
+        //Arrange
+        Region region = new Region("Region", null);
+
+        //Assert
+        assertEquals("Region", region.getName());
+    }
+
+    @Test
+    void getCountries() { // Arrange
+        ArrayList<Country> countries = new ArrayList<>();
+        countries.add(new Country("ABC", "Country1", null, null, null, 1));
+        countries.add(new Country("DEF", "Country2", null, null, null, 2));
+        countries.add(new Country("GHI", "Country3", null, null, null, 3));
+
+        Region region = new Region("Region", countries);
+
+        // Assert
+        assertEquals(countries, region.getCountries());
+    }
+
+    @Test
+    void getPopulation() {
+        ArrayList<Country> countries = new ArrayList<>();
+        countries.add(new Country("ABC", "Country1", null, null, null, 1));
+        countries.add(new Country("DEF", "Country2", null, null, null, 2));
+        countries.add(new Country("GHI", "Country3", null, null, null, 3));
+
+        Region region = new Region("Region", countries);
+
+        // Assert
+        assertEquals(6, region.getPopulation());
+    }
+}
