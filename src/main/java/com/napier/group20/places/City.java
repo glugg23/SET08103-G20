17c47474a5c270d91458ba6f5d4d7fca779296ea
glugg23 <m.leonhardt424@gmail.com>
2019-03-08 13:00:23 +0000

    Removed references to District and Country in City

--- src/main/java/com/napier/group20/places/City.java
+++ src/main/java/com/napier/group20/places/City.java
@@ -5,37 +5,25 @@ import com.napier.group20.utils.Population;
 /**
  * City.java
  *
- * Stores information about a city including its name, country,
- * district, population and if it is the capital city.
+ * Stores information about a city including its name,
+ * population and if it is the capital city.
  *
  */
 
 public class City implements Population {
 
     private String name;
-    private Country country;
-    private District district;
     private int population;
     private boolean isCapital;
 
-    public City(String name, Country country, District district, int population, boolean isCapital) {
+    public City(String name, int population, boolean isCapital) {
         this.name = name;
-        this.country = country;
-        this.district = district;
         this.population = population;
         this.isCapital = isCapital;
     }
 
     public String getName() {
         return name;
-    }
-
-    public Country getCountry() {
-        return country;
-    }
-
-    public District getDistrict() {
-        return district;
     }
 
     public long getPopulation() {
--- src/test/java/com/napier/group20/places/CountryTest.java
+++ src/test/java/com/napier/group20/places/CountryTest.java
@@ -4,7 +4,8 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 class CountryTest {
 
@@ -57,12 +58,12 @@ class CountryTest {
     void getCitiesPopulation() {
         // Arrange
         ArrayList<City> cities1 = new ArrayList<>();
-        cities1.add(new City("City1", null, null, 100,true));
-        cities1.add(new City("City2", null, null, 50,false));
+        cities1.add(new City("City1", 100,true));
+        cities1.add(new City("City2", 50,false));
 
         ArrayList<City> cities2 = new ArrayList<>();
-        cities2.add(new City("City3", null, null, 100,false));
-        cities2.add(new City("City4", null, null, 50,false));
+        cities2.add(new City("City3", 100,false));
+        cities2.add(new City("City4", 50,false));
 
         ArrayList<District> districts = new ArrayList<>();
         districts.add(new District("District1", cities1));
@@ -77,7 +78,7 @@ class CountryTest {
     @Test
     void getCapital() {
         // Arrange
-        City capital = new City("City1", null, null, 100,true);
+        City capital = new City("City1",100,true);
 
         Country country = new Country("ABC", "Country", null, capital, null,
                 12345);
